3038eec733f59d81f1d071200af5209b7ee05a7c
cmp <devbycm@gmail.com>
2018-09-06 11:11:24 +1000

    update

--- logistic.py
+++ logistic.py
@@ -1,64 +1,60 @@
 import csv
-
-from sklearn.linear_model import LogisticRegression
 import numpy as np
-
-# features_path = "data/f3.txt"
-# test_path = "data/test-public.txt"
-
-# tp_path = "X1.csv"
-# pn_path = "X2.csv"
-# t_path = "X3.csv"
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 
-tp_path = "out_combined/tp.csv"
-pn_path = "out_combined/pn.csv"
-t_path = "out_combined/t_.csv"
 
-# rng = (4, 6, 14)  # 0.75505, 0.6753
-# rng = (3, 6)  # 0.77909, 0.6743
-# rng = (3, 6, 10)  # 0.78365, 0.6729
+def output_pred(path, pred):
+    with open(path, 'w', newline='') as f:
+        w = csv.writer(f, delimiter=',')
+        w.writerow(['Id', 'Prediction'])
+        for i, p in enumerate(pred[:, 1]):
+            w.writerow([i + 1, p])
 
-# rng = (3, 6, 10, 16)
 
-rng = (3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17)
+def train_and_pred(rng, test=False):
+    tp_path = "out_combined/tp.csv"
+    pn_path = "out_combined/pn.csv"
+    t_path = "out_combined/t_.csv"
 
-X1 = np.genfromtxt(tp_path, usecols=rng, skip_header=1, delimiter=",")
-X2 = np.genfromtxt(pn_path, usecols=rng, skip_header=1, delimiter=",")
-XT = np.genfromtxt(t_path, usecols=rng, skip_header=1, delimiter=",")
+    X1 = np.genfromtxt(tp_path, usecols=rng, skip_header=1, delimiter=",")
+    X2 = np.genfromtxt(pn_path, usecols=rng, skip_header=1, delimiter=",")
+    XT = np.genfromtxt(t_path, usecols=rng, skip_header=1, delimiter=",")
 
-print(X1.shape)
-print(X2.shape)
-print(XT.shape)
+    print(X1.shape)
+    print(X2.shape)
+    print(XT.shape)
 
-y1 = np.ones(len(X1))
-y2 = np.zeros(len(X2))
+    y1 = np.ones(len(X1))
+    y2 = np.zeros(len(X2))
 
-X = np.concatenate((X1, X2))
-y = np.concatenate((y1, y2))
+    X = np.concatenate((X1, X2))
+    y = np.concatenate((y1, y2))
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=666)
+    y_test = None
+    if test:
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=90054)
+    else:
+        X_train, y_train = X, y
+        X_test = XT
 
-X_train, y_train = X, y
-X_test = XT
+    clf = LogisticRegression(C=1)
+    clf.fit(X_train, y_train)
 
-clf = LogisticRegression(C=1)
-clf.fit(X_train, y_train)
+    pred = clf.predict_proba(X_test)
 
-pred = clf.predict_proba(X_test)
+    if y_test is not None:
+        roc = roc_auc_score(y_test, pred[:, 1])
+        print(roc)
+        print()
 
-# roc = roc_auc_score(y_test, pred[:, 1])
-# print(roc)
+    return pred
 
-# print()
-# for v in pred[:, 1]:
-# print(v)
 
-with open('pred.csv', 'w', newline='') as f:
-    w = csv.writer(f, delimiter=',')
-    w.writerow(['Id', 'Prediction'])
-    for i, p in enumerate(pred[:, 1]):
-        w.writerow([i + 1, p])
+def train_and_test(rng):
+    return train_and_pred(rng, True)
 
-print(np.mean(pred[:, 1]))
+
+def calc_pred_mean(pred):
+    print(np.mean(pred[:, 1]))
--- scripts/main.py
+++ scripts/main.py
@@ -1,18 +1,29 @@
 import generate_feature as gf
 import method_undirected as mud
 import method_directed as md
+import combine_csv as ccsv
 
 tp_c = gf.Config("../train/tp_pairs_20000.csv", "tp")
 pn_c = gf.Config("../train/pn_pairs_20000.csv", "pn")
 t_c = gf.Config("../train/test-public.txt", "t")
 all_c = [tp_c, pn_c, t_c]
 
-t = gf.GenerateFeatureTask(out_dir="../out_single", out_suffix="csv")
-t.load_graph("udg.gpickle")
-# t.load_graph("dg.gpickle")
 
-# t.multi_load_and_output([tp_c, t_c], [md.z1_b_to_a, md.z2_middle])
-# t.multi_load_and_output([pn_c], md.methods)
-# t.multi_load_and_output(all_c, md.methods)
-t.multi_load_and_output([pn_c], mud.methods)
+def generate_feature_csv():
+    t = gf.GenerateFeatureTask(out_dir="../out_single", out_suffix="csv")
+    t.load_graph("udg.gpickle")
+    # t.load_graph("dg.gpickle")
 
+    # t.multi_load_and_output([tp_c, t_c], [md.z1_b_to_a, md.z2_middle])
+    # t.multi_load_and_output([pn_c], md.methods)
+    # t.multi_load_and_output(all_c, md.methods)
+    t.multi_load_and_output([pn_c], mud.methods)
+
+
+def main():
+    generate_feature_csv()
+    ccsv.multi_combine_csv("../out_single", "../out_combined", ["pn", "tp", "t"])
+
+
+if __name__ == "__main__":
+    main()
